e96eb6c328083178d09cf2937c62c7ebe7662534
DeikuModder <gabrieltc555@gmail.com>
2023-08-17 21:20:43 -0400

    Responsive navbar, discord id copy added

--- script.js
+++ script.js
@@ -1,7 +1,27 @@
-document.querySelector('#clipboard').onclick = async () => {
+const discordbtn = document.getElementById("clipboard")
+
+discordbtn.addEventListener("click", async () => {
     try {
         await navigator.clipboard.writeText('gab3073')
     } catch (error) {
-        console.log(error);
+        console.log('error')
     }
-}
+
+    window.alert("Discord ID copied to clipboard!")
+})
+
+const navBtn = document.getElementById("btnToggle");
+const navBar = document.getElementById("navicons");
+
+navBtn.addEventListener('click', () => {
+    const visibility = navBar.getAttribute("data-visible");
+
+    if (visibility === "false"){
+        navBar.setAttribute("data-visible", true);
+        navBtn.setAttribute("aria-expanded", true);
+    }
+    else {
+        navBar.setAttribute("data-visible", false);
+        navBtn.setAttribute("aria-expanded", false);
+    }
+})
